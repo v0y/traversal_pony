1ff10d3a6c2c1f65239faf9e6674e50ff5a41d5e
Rafał Mirończyk <voyageur.pl@gmail.com>
2015-01-16 13:58:22 +0100

    add db.py

---
+++ pony/pony/db.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from zope.sqlalchemy import ZopeTransactionExtension
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+
+
+def get_session():
+    session = scoped_session(sessionmaker(extension=None))
+    session.configure(bind=DBSession.get_bind())
+    return session
+
+
+class BaseModel(object):
+
+    @classmethod
+    def by_id(cls, id_):
+        return DBSession.query(cls).filter_by(id=id_).first()
+
+
+Base = declarative_base(cls=BaseModel)
