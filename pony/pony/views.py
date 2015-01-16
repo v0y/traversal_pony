944af968626f71465714e9db31b9d88bcfadd2f5
Rafał Mirończyk <voyageur.pl@gmail.com>
2015-01-16 16:27:08 +0100

    just checking if git is working properly...

--- pony/pony/views.py
+++ pony/pony/views.py
@@ -9,6 +9,21 @@ def my_view(request):
     return {'project': 'pony'}
 
 
+@view_config(name='all', context=Kind, renderer='templates/mytemplate.pt')
+def my_view_k(request):
+    return {'project': 'szystkie kindy'}
+
+
+@view_config(name='all', context=Pony, renderer='templates/mytemplate.pt')
+def my_view_p(request):
+    return {'project': 'szystkie koniki'}
+
+
+@view_config(name='all', context=Group, renderer='templates/mytemplate.pt')
+def my_view_g(request):
+    return {'project': 'szystkie koniki grupy {}'.format(request.context.name)}
+
+
 @view_config(context=Group, renderer='templates/mytemplate.pt')
 def my_view2(request):
     return {'project': [p.name for p in request.context.ponies]}
@@ -17,6 +32,11 @@ def my_view2(request):
 @view_config(context=Kind, renderer='templates/mytemplate.pt')
 def my_view3(request):
     return {'project': [p.name for p in request.context.ponies]}
+
+
+@view_config(name='all', renderer='templates/mytemplate.pt')
+def my_view666(request):
+    return {'project': 'szystkie blble {}'.format(request.context)}
 
 
 @view_config(context=Pony, renderer='templates/mytemplate.pt')
@@ -29,10 +49,6 @@ def widok_konika(request):
 
 
 def get_groups():
-    # return {
-    #     'a': {},
-    #     'b': {},
-    # }
     groups_dict = {}
     for g in DBSession.query(Group).all():
         groups_dict[g.name] = g
@@ -44,6 +60,7 @@ def get_kinds():
     for k in DBSession.query(Kind).all():
         kinds_dict[k.name] = k
     return kinds_dict
+
 
 def get_root(request):
     return {
